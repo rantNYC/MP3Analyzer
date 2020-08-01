94054b4d81f596a18838e33d03cac08c2e34318c
Rafael <r.ninalaya@outlook.com>
2020-08-01 00:44:03 -0400

    Add property file and modify GUI to accomodate

--- src/main/java/com/projects/mp3/view/MainGUI.java
+++ src/main/java/com/projects/mp3/view/MainGUI.java
@@ -1,7 +1,11 @@
 package com.projects.mp3.view;
 
+import java.net.URL;
+import java.util.Properties;
+
+import com.projects.mp3.controller.storage.mysql.MySQLDriver;
 import com.projects.mp3.view.stages.GUIStage;
-import com.projects.mp3.view.stages.LoginStage;
+import com.projects.mp3.view.stages.MainGUIStage;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -14,7 +18,21 @@ public class MainGUI extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		GUIStage login = new LoginStage(453, 214, getClass().getResource("/FXML/MainDBGUI.fxml"));
+		try {
+		URL rootPath = getClass().getResource("/app.properties");
+		 
+		Properties appProps = new Properties();
+		appProps.load(rootPath.openStream());
+	
+		MySQLDriver driver = new MySQLDriver(appProps.getProperty("app.mysql.hostname"),
+												appProps.getProperty("app.mysql.username"),
+												appProps.getProperty("app.mysql.password"));
+		
+		GUIStage login = new MainGUIStage(900, 600, driver, getClass().getResource("/FXML/MainGUI.fxml"));
 		login.startStage();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+		}
 	}
 }
